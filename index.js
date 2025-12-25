53a9cbc9da7e13e6d4eb13412cf9549efc90d08b
Dean Howe <hideanhowe@gmail.com>
2025-12-25 00:33:29 +0000

    testing

--- index.js
+++ index.js
@@ -8,3 +8,36 @@
 // It contains at least one number.
 // It contains at least one special character from this set: !, @, #, $, %, ^, &, or *.
 // Return "weak" if the password meets fewer than two of the rules. Return "medium" if the password meets 2 or 3 of the rules. Return "strong" if the password meets all 4 rules.
+
+function checkStrength(password) {
+  console.log(password);
+
+  const counter = 0;
+
+  // it is at least 8 characters long
+  if (password.length >= 8) {
+    counter++;
+  }
+
+  // contains both lower & uppercase
+  const upper = /[A-Z]/;
+  const lower = /[a-z]/;
+  if (upper.test(password) || lower.test(password)) {
+    counter++;
+  }
+
+  // contains at least one number
+  const num = /[0-9]/;
+  if (num.test(password)) {
+    counter++;
+  }
+
+  // contains at least one special character
+  const chars = /[!@#$%\^\&\*]/;
+  if (chars.test(password)) {
+    counter++;
+  }
+
+  console.log(counter);
+  return password;
+}
